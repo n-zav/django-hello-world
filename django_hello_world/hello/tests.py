5919f8a505e46be07fa3f23978d6211c43738306
n-zav <nastya.zavalkina@gmail.com>
2013-11-25 22:21:46 +0100

    ticket:1 pep8 adjustment

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -44,10 +44,14 @@ class TestSignup(unittest.TestCase):
 
     def test_signup_fire(self):
         self.driver.get(settings.SESSION_COOKIE_DOMAIN + ':8000')
-        self.assertEqual('Nastya', self.driver.find_element_by_id('name').text)
-        self.assertEqual('nastya.zavalkina@gmail.com', self.driver.find_element_by_id('email').text)
-        self.assertEqual('nastya.zavalkina', self.driver.find_element_by_id('skype').text)
-        self.assertIn(settings.SESSION_COOKIE_DOMAIN + ':8000', self.driver.current_url)
+        self.assertEqual('Nastya',
+                         self.driver.find_element_by_id('name').text)
+        self.assertEqual('nastya.zavalkina@gmail.com',
+                         self.driver.find_element_by_id('email').text)
+        self.assertEqual('nastya.zavalkina',
+                         self.driver.find_element_by_id('skype').text)
+        self.assertIn(settings.SESSION_COOKIE_DOMAIN + ':8000',
+                      self.driver.current_url)
 
     def tearDown(self):
         self.driver.quit()

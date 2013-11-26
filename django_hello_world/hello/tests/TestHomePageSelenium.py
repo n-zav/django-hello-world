efbfe1449cbc403b627f9e7aad922b7ff86c65e5
n-zav <nastya.zavalkina@gmail.com>
2013-11-26 12:07:38 +0100

    ticket:1 Without selenium test

--- django_hello_world/hello/tests/TestHomePageSelenium.py
+++ django_hello_world/hello/tests/TestHomePageSelenium.py
@@ -1,25 +1,27 @@
-from django.conf import settings
-from selenium import webdriver
-import unittest
+#  Uncomment for selenium test
 
-
-class TestHomePageSelenium(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-
-    def test_homepage(self):
-        self.driver.get(settings.SESSION_COOKIE_DOMAIN + ':8000')
-        self.assertEqual('Nastya',
-                         self.driver.find_element_by_id('name').text)
-        self.assertEqual('nastya.zavalkina@gmail.com',
-                         self.driver.find_element_by_id('email').text)
-        self.assertEqual('nastya.zavalkina',
-                         self.driver.find_element_by_id('skype').text)
-        self.assertIn(settings.SESSION_COOKIE_DOMAIN + ':8000',
-                      self.driver.current_url)
-
-    def tearDown(self):
-        self.driver.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+# from django.conf import settings
+# from selenium import webdriver
+# import unittest
+#
+#
+# class TestHomePageSelenium(unittest.TestCase):
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
+#
+#     def test_homepage(self):
+#         self.driver.get(settings.SESSION_COOKIE_DOMAIN + ':8000')
+#         self.assertEqual('Nastya',
+#                          self.driver.find_element_by_id('name').text)
+#         self.assertEqual('nastya.zavalkina@gmail.com',
+#                          self.driver.find_element_by_id('email').text)
+#         self.assertEqual('nastya.zavalkina',
+#                          self.driver.find_element_by_id('skype').text)
+#         self.assertIn(settings.SESSION_COOKIE_DOMAIN + ':8000',
+#                       self.driver.current_url)
+#
+#     def tearDown(self):
+#         self.driver.quit()
+#
+# if __name__ == '__main__':
+#     unittest.main()
--- django_hello_world/hello/tests/__init__.py
+++ django_hello_world/hello/tests/__init__.py
@@ -1,9 +1,7 @@
 from django_hello_world.hello.tests.HomePageViewTestCase import *
-from django_hello_world.hello.tests.TestHomePageSelenium import *
 
   
 #starts the test suite  
 __test__= {  
            'homepage_test': HomePageViewTestCase,
-           'selenium_test': TestHomePageSelenium
            }

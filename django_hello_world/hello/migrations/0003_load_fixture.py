41fbf380a2c35c2824938d18fbf5681c3e325176
n-zav <nastya.zavalkina@gmail.com>
2013-11-27 12:23:49 +0100

    ticket:1 South migration to load initial data from fixture

---
+++ django_hello_world/hello/migrations/0003_load_fixture.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        from django.core.management import call_command
+        call_command("loaddata", "initial_data.json")
+        # Note: Don't use "from appname.models import ModelName".
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        'hello.person': {
+            'Meta': {'object_name': 'Person'},
+            'biography': ('django.db.models.fields.TextField', [], {}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        }
+    }
+
+    complete_apps = ['hello']
+    symmetrical = True

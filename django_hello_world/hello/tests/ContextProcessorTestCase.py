70e33cf9ddff539f12b3edb348dfc749b0283a41
n-zav <nastya.zavalkina@gmail.com>
2013-11-29 20:34:36 +0100

    ticket:4 covered template context processor with unittest

---
+++ django_hello_world/hello/tests/ContextProcessorTestCase.py
@@ -0,0 +1,17 @@
+from django.template import RequestContext
+from django.test.client import RequestFactory
+from ..context_processors import access_to_settings
+from django.conf import settings
+import unittest
+
+
+class ContextProcessorTestCase(unittest.TestCase):
+
+    def test_access_to_settings(self):
+        """
+        Tests that settings are in context
+        """
+        request = RequestFactory().get('/')
+        context = RequestContext(request, {}, [access_to_settings])
+        self.assertIn('SETTINGS', context)
+        self.assertEquals(context['SETTINGS'], settings)

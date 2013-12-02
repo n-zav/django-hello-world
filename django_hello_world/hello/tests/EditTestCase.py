d1013dbedebac38568418f7fc45c13802e0b4a12
n-zav <nastya.zavalkina@gmail.com>
2013-12-02 21:51:14 +0100

    ticket:5 added edit page tests

--- django_hello_world/hello/tests/EditTestCase.py
+++ django_hello_world/hello/tests/EditTestCase.py
@@ -1,17 +1,86 @@
 from django.test import TestCase
-from ..forms import EditForm
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 from ..models import Person
+from unittest_data_provider import data_provider
 
 
-class EditTestCase(TestCase):
+class NonAuthEditTestCase(TestCase):
+    def test_edit_page_redirect(self):
+        response = self.client.get(reverse('edit-view'))
+        self.assertEqual(response.status_code, 302)
 
-    def test_valid_form(self):
-        person = Person.objects.create(first_name="Jane",
-                                       last_name="Doe",
-                                       date_of_birth="2001-01-01",
-                                       biography="Jane Doe's bio",
-                                       email="jane.doe@example.com",
-                                       jabber="jane.doe@jabber.ru",
-                                       skype="jane.doe",
-                                       other_contacts="Contact Jane",
-                                       image="")
+    def test_edit_page_redirects_to_home_page(self):
+        response = self.client.get(reverse('edit-view'), follow=True)
+        last_redirect = response.redirect_chain[-1][0]
+        self.assertIn("/accounts/login/?next=/edit/", last_redirect)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('<title>Log in here</title>', response.content)
+
+
+class AuthEditTestCase(TestCase):
+    def setUp(self):
+        self.person = Person.objects.get(pk=1)
+        self.user = User.objects.create_user('Jane', 'jane.doe@gmail.com',
+                                             'test')
+        self.client.login(username='Jane', password='test')
+        self.response = self.client.get(reverse('edit-view'))
+
+    page_elements = lambda: (
+        ('<form',),
+        ('Cancel',)
+    )
+
+    @data_provider(page_elements)
+    def test_edit_page(self, page_element):
+        # test that the user can login.
+        self.assertEqual(self.response.status_code, 200)
+        # test that page elements exist on /edit/ page.
+        self.assertIn(page_element, self.response.content)
+
+    form_fields = lambda: (
+        ('first_name',),
+        ('last_name',),
+        ('biography',),
+        ('email',),
+        ('jabber',),
+        ('skype',),
+        ('other_contacts',),
+    )
+
+    @data_provider(form_fields)
+    def test_edit_form(self, form_field):
+        self.assertIn(getattr(self.person, form_field), self.response.content)
+
+
+class EditContactTestCasePost(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('Jane', 'jane.doe@gmail.com',
+                                             'test')
+        self.client.login(username='Jane', password='test')
+        self.proper_data = {"first_name": "Nastya",
+                            "last_name": "Zavalkina",
+                            "date_of_birth": "1990-08-06",
+                            "biography": "Here I am",
+                            "email": "nastya.zavalkina@gmail.com",
+                            "jabber": "nastya.zavalkina@jabber.ru",
+                            "skype": "nastya.zavalkina",
+                            "other_contacts": "twitter: @mirrronenko"}
+
+    def test_form_submit_no_data(self):
+        response = self.client.post(reverse('edit-view'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['form']['first_name'].errors,
+                         [u'This field is required.'])
+
+    def test_form_submit_with_data(self):
+        new_name = "Martishka"
+        self.proper_data['first_name'] = new_name
+        self.client.post(reverse('edit-view'), self.proper_data)
+        person = Person.objects.get(pk=1)
+        self.assertEqual(person.first_name, new_name)
+
+    def test_form_submit_redirect(self):
+        response = self.client.post(reverse('edit-view'), self.proper_data,
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
--- django_hello_world/hello/tests/__init__.py
+++ django_hello_world/hello/tests/__init__.py
@@ -3,13 +3,3 @@ from .RequestTestCase import *
 from .ContextProcessorTestCase import *
 from .EditTestCase import *
 from .AuthTestCase import *
-
-  
-#starts the test suite
-__test__= {  
-           'homepage_test': HomePageViewTestCase,
-           'request_test': RequestTestCase,
-           'processor_test': ContextProcessorTestCase,
-           'edit_form_test': EditTestCase,
-           'auth_test': AuthTestCase
-           }

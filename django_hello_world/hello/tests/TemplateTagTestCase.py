f0c481b07e8905ef498fb972bdde19ee484eb30f
n-zav <nastya.zavalkina@gmail.com>
2013-12-04 02:25:00 +0100

    t8: fixed template tag test

--- django_hello_world/hello/tests/TemplateTagTestCase.py
+++ django_hello_world/hello/tests/TemplateTagTestCase.py
@@ -4,16 +4,16 @@ from ..templatetags.object_admin_edit_link_tag import admin_edit_link
 
 
 class TemplateTagTestCase(TestCase):
-
-    person = Person.objects.create(pk=2,
-                                   first_name='Jane',
-                                   last_name='Doe',
-                                   date_of_birth='2001-01-01',
-                                   biography='Jane Doe',
-                                   email='jane.oe@example.com',
-                                   jabber='jane.doe.jabber.ru',
-                                   skype='jane.doe',
-                                   other_contacts='Contact Jane')
+    def setUp(self):
+        self.person = Person.objects.create(pk=2,
+                                            first_name='Jane',
+                                            last_name='Doe',
+                                            date_of_birth='2001-01-01',
+                                            biography='Jane Doe',
+                                            email='jane.oe@example.com',
+                                            jabber='jane.doe.jabber.ru',
+                                            skype='jane.doe',
+                                            other_contacts='Contact Jane')
 
     def test_edit_admin_link(self):
         self.assertEqual(admin_edit_link(self.person), '/admin/hello/person/2/')
